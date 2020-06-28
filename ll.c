9d1c7ed389dc676b7c6ef6b3c37d566cdb104bdd
Sourabh Keshri <42672283+sourabhkeshri@users.noreply.github.com>
2020-06-28 14:40:15 +0530

    Add files via upload
    Linked List

---
+++ ll.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct Node{
+int data;
+struct Node *next;
+}*first=NULL;
+
+void create(int A[], int n){
+struct Node *t,*last;
+first=(struct Node*)malloc(sizeof(struct Node));
+first->data=A[0];
+first->next=NULL;
+last=first;
+
+for(int i=1;i<n;i++){
+    t=(struct Node*)malloc(sizeof(struct Node));
+    t->data=A[i];
+    t->next=NULL;
+    last->next=t;
+    last=t;
+}
+}
+
+//Display nodes
+void display(struct Node*p){
+
+printf("The list is\n");
+while(p!=NULL){
+    printf("%d ",p->data);
+    p=p->next;
+}
+
+}
+
+int maxInList(struct Node *p){
+int m=0;
+while(p!=NULL){
+    if(p->data>m)
+        m=p->data;
+        p=p->next;
+}
+return m;
+}
+
+int main(){
+int A[]={12,8,9,776,66};
+create(A,5);
+display(first);
+int m=maxInList(first);
+printf("max is :%d",m);
+
+}
+
+
